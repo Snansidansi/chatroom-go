d88c8f6fa7ce8f7c047f1cf56a87a7b11271c3b7
Snansidansi <165903080+Snansidansi@users.noreply.github.com>
2025-08-10 20:18:34 +0200

    added popup styling

--- react-chatroom-client/src/connectPopup.tsx
+++ react-chatroom-client/src/connectPopup.tsx
@@ -26,16 +26,18 @@ function ConnectPopup(props: ConnectPopupProps) {
     <div className="popup">
       <div className="popup-inner">
         <h1>Connect to Chatroom</h1>
-        <div>
-          IP with Port:
+        <div className="popup-input">
+          <label htmlFor="ip">IP with Port:</label>
           <input
             type="text"
+            id="ip"
             value={ip}
             onChange={(e) => setIp(e.target.value)}
           />
-          Name:
+          <label htmlFor="name">Name:</label>
           <input
             type="text"
+            id="name"
             value={userName}
             onChange={(e) => setUserName(e.target.value)}
           />
