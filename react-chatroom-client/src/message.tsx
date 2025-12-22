a6a4f18eafa73b8ae54d1e507d10e0e1d9012d05
Snansidansi <165903080+Snansidansi@users.noreply.github.com>
2025-12-22 23:15:54 +0100

    added colors to name on website (based on hash)

--- react-chatroom-client/src/message.tsx
+++ react-chatroom-client/src/message.tsx
@@ -3,10 +3,22 @@ export type MessageProps = {
   text: string;
 };
 
+function stringToHslColor(str: string, s: number, l: number) {
+  let hash = 0;
+  for (let i = 0; i < str.length; i++) {
+    hash = str.charCodeAt(i) + ((hash << 5) - hash);
+  }
+
+  const h = hash % 360;
+  return `hsl(${h}, ${s}%, ${l}%)`;
+}
+
 function Message(props: MessageProps) {
+  const nameColor = stringToHslColor(props.name, 70, 50);
+
   return (
     <>
-      <strong>{props.name}</strong>: {props.text}
+      <strong style={{ color: nameColor }}>{props.name}</strong>: {props.text}
     </>
   );
 }
